d2a3e2ac2b4765d0cbc96f58e5182281be005aca
Muhammad Ali <ali@muhammada.li>
2016-05-31 06:41:40 -0400

    Hook up library for OAuth authentication

--- app/src/main/java/li/muhammada/rbc/yelp/provider/YelpRetrofit.java
+++ app/src/main/java/li/muhammada/rbc/yelp/provider/YelpRetrofit.java
@@ -5,8 +5,11 @@ import android.util.Log;
 
 import java.io.IOException;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class YelpRetrofit {
 
@@ -14,10 +17,24 @@ public class YelpRetrofit {
     public static YelpRetrofit INSTANCE = new YelpRetrofit();
     private final YelpService mService;
 
+    /* In a production quality app, these keys should be obfuscated */
+    private static final String CONSUMER_KEY = "QAp3lKE-klN0GM686xHiAg";
+    private static final String CONSUMER_SECRET = "NmDyBwNDArfHqPMIn9-npfi0yAw";
+    private static final String TOKEN = "-FbwFOTtKurTX6JUHhCSjaIZGnoJB9Xc";
+    private static final String TOKEN_SECRET = "jxoxGWkclNSDE41aiG1-Ox0e0tE";
+
     private YelpRetrofit() {
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+        consumer.setTokenWithSecret(TOKEN, TOKEN_SECRET);
+
+        OkHttpClient okClient = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer))
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.yelp.com/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okClient)
                 .build();
         mService = retrofit.create(YelpService.class);
     }
