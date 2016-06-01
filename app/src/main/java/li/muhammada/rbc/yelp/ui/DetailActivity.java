0eeb603912ad2ed37cb141b34a07917322d31a6a
Muhammad Ali <ali@muhammada.li>
2016-06-01 08:18:46 -0400

    Make a layout for the details page and fill it in with some data

--- app/src/main/java/li/muhammada/rbc/yelp/ui/DetailActivity.java
+++ app/src/main/java/li/muhammada/rbc/yelp/ui/DetailActivity.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import li.muhammada.rbc.yelp.R;
 import li.muhammada.rbc.yelp.provider.model.Business;
 
@@ -15,19 +21,23 @@ public class DetailActivity extends AppCompatActivity {
 
     private Business mBusiness;
 
+    @BindView(R.id.hero_imageview) ImageView heroImageView;
+    @BindView(R.id.rating_imageview) ImageView ratingImageView;
+    @BindView(R.id.rating_count) TextView ratingCount;
+    @BindView(R.id.avatar) ImageView avatar;
+    @BindView(R.id.snippet_text) TextView snippetText;
+    @BindView(R.id.address) TextView address;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(view -> {});
-        }
 
         handleIntent(getIntent());
     }
@@ -53,7 +63,26 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        //noinspection ConstantConditions
         getSupportActionBar().setTitle(mBusiness.getName());
+        PicassoHelper.INSTANCE.picasso().load(mBusiness.getImageUrl()).into(heroImageView);
+        PicassoHelper.INSTANCE.picasso().load(mBusiness.getRatingImgUrlLarge()).into(ratingImageView);
+        ratingCount.setText(String.format(Locale.getDefault(), getString(R.string.brackets), mBusiness.getReviewCount()));
+        PicassoHelper.INSTANCE.picasso().load(mBusiness.getSnippetImageUrl()).into(avatar);
+        snippetText.setText(mBusiness.getSnippetText());
+        address.setText(getAddress());
+    }
+
+    private String getAddress() {
+        if (mBusiness == null || mBusiness.getLocation() == null) {
+            return "";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (String line : mBusiness.getLocation().getDisplayAddress()) {
+            builder.append(line).append('\n');
+        }
+        return builder.toString();
     }
 
 }
