11ccffe4c49cefe4a77a13555069798d3b04dea0
Muhammad Ali <ali@muhammada.li>
2016-06-01 08:42:01 -0400

    Make the address in the detail activity clickable

--- app/src/main/java/li/muhammada/rbc/yelp/ui/DetailActivity.java
+++ app/src/main/java/li/muhammada/rbc/yelp/ui/DetailActivity.java
@@ -1,13 +1,15 @@
 package li.muhammada.rbc.yelp.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.net.URI;
 import java.util.Locale;
 
 import butterknife.BindView;
@@ -19,7 +21,7 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String INTENT_PARAM_BUSINESS = "Business";
 
-    private Business mBusiness;
+    private Business business;
 
     @BindView(R.id.hero_imageview) ImageView heroImageView;
     @BindView(R.id.rating_imageview) ImageView ratingImageView;
@@ -54,35 +56,51 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void handleIntent(Intent intent) {
-        mBusiness = ((Business) intent.getSerializableExtra(INTENT_PARAM_BUSINESS));
+        business = ((Business) intent.getSerializableExtra(INTENT_PARAM_BUSINESS));
         loadPage();
     }
 
     private void loadPage() {
-        if (mBusiness == null) {
+        if (business == null) {
             return;
         }
 
         //noinspection ConstantConditions
-        getSupportActionBar().setTitle(mBusiness.getName());
-        PicassoHelper.INSTANCE.picasso().load(mBusiness.getImageUrl()).into(heroImageView);
-        PicassoHelper.INSTANCE.picasso().load(mBusiness.getRatingImgUrlLarge()).into(ratingImageView);
-        ratingCount.setText(String.format(Locale.getDefault(), getString(R.string.brackets), mBusiness.getReviewCount()));
-        PicassoHelper.INSTANCE.picasso().load(mBusiness.getSnippetImageUrl()).into(avatar);
-        snippetText.setText(mBusiness.getSnippetText());
-        address.setText(getAddress());
+        getSupportActionBar().setTitle(business.getName());
+        PicassoHelper.INSTANCE.picasso().load(business.getImageUrl()).into(heroImageView);
+        PicassoHelper.INSTANCE.picasso().load(business.getRatingImgUrlLarge()).into(ratingImageView);
+        ratingCount.setText(String.format(Locale.getDefault(), getString(R.string.brackets), business.getReviewCount()));
+        PicassoHelper.INSTANCE.picasso().load(business.getSnippetImageUrl()).into(avatar);
+        snippetText.setText(business.getSnippetText());
+        setupAddress();
     }
 
-    private String getAddress() {
-        if (mBusiness == null || mBusiness.getLocation() == null) {
-            return "";
+    private void setupAddress() {
+        if (business == null || business.getLocation() == null) {
+            address.setText("");
+            return;
         }
 
         StringBuilder builder = new StringBuilder();
-        for (String line : mBusiness.getLocation().getDisplayAddress()) {
+        for (String line : business.getLocation().getDisplayAddress()) {
             builder.append(line).append('\n');
         }
-        return builder.toString();
+        address.setText(builder.toString());
+
+        if (business.getLocation().getCoordinate() != null) {
+            address.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+            address.setOnClickListener(v -> {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                String uri = String.format(Locale.US, "geo:%f,%f?q=%s",
+                        business.getLocation().getCoordinate().getLatitude(),
+                        business.getLocation().getCoordinate().getLongitude(),
+                        Uri.encode(builder.toString()));
+                intent.setData(Uri.parse(uri));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
 }
